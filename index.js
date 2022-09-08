8072c4d3a132c73d3a18f193adbaf9c869aa3801
Mohammed Ali <ghostrepulsive@gmail.com>
2022-09-08 07:41:28 +0300

    inital commit of the js course
    suck it

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.getElementById (count).innerText = 23
+
+let ini = 50
+
+count = ini + 50
+// = 100
+console.log (count)
+
+count = ini - 25
+// = 25 
+console.log (count)
+
+count = ini + 25 
+// = 75 
+console.log (count)
+
+
+
